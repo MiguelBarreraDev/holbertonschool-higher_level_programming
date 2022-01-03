11e7ce1988875fbf40b0d6761353f36d18795fd5
MiguelBarreraDev <barreram987@gmail.com>
2022-01-03 14:23:22 -0500

    Write a function that adds 2 tuples.

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    first = int
-    second = int
-    if len(tuple_a) >= 2 and len(tuple_b) >= 2:
-        first = tuple_a[0] + tuple_b[0]
-        second = tuple_a[1] + tuple_b[1]
-    elif len(tuple_a) >= 2 and len(tuple_b) < 2:
-        if len(tuple_b) > 0:
-            first = tuple_a[0] + tuple_b[0]
-            second = tuple_a[1] + 0
-        else:
-            first = tuple_a[0]
-            second = tuple_a[1]
-    elif len(tuple_a) < 2 and len(tuple_b) >= 2:
-        if len(tuple_a) > 0:
-            first = tuple_a[0] + tuple_b[0]
-            second = 0 + tuple_b[1]
-        else:
-            first = tuple_b[0]
-            second = tuple_b[1]
+    len_a == len(tuple_a)
+    len_b == len(tuple_b)
 
-    return (first, second)
+    if len_a == 0:
+        a1 = 0
+        a2 = 0
+    elif len_a == 1:
+        a1 = tuple_a[0]
+        a2 = 0
+    else:
+        a1 = tuple_a[0]
+        a2 = tuple_a[1]
+
+    if len_b == 0:
+        b1 = 0
+        b2 = 0
+    elif len_b == 1:
+        b1 = tuple_b[0]
+        b2 = 0
+    else:
+        b1 = tuple_b[0]
+        b2 = tuple_b[1]
+
+    return (a1 + b1, a2 + b2)
