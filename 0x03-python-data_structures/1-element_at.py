9d6b8e20b9e78658dcb30c6a69909dc3b6d28eeb
MiguelBarreraDev <barreram987@gmail.com>
2022-01-03 12:47:41 -0500

    space around - line 3

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 def element_at(my_list, idx):
     if idx <= 0:
         return None
-    if idx >= len(my_list)
+    if idx >= len(my_list):
         return None
     e = my_list[idx]
     return e
