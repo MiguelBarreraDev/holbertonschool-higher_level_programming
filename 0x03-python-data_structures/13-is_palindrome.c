7b7576b15efbd6c72a03b6dfe4fea937fb18d692
MiguelBarreraDev <barreram987@gmail.com>
2022-01-03 16:32:43 -0500

    lists.h

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,65 @@
+#include "lists.h"
+/**
+* betty comentary
+*/
+int is_palindrome(listint_t **head)
+{
+	listint_t *first, *end;
+	int band = 0; 
+	size_t len = 0, count = 0;
+
+	if (!head || *head == NULL)
+		return (band);
+
+	first = *head;
+	end = reversed_list(head);
+	len = len_list(head);
+
+	while(first != NULL)
+	{
+		if (first->n == end->n)
+			count++;
+		first = first->next;
+		end = end->next;
+	}
+
+	band = (count == len) ? 1 : 0;
+	
+	return (band); 
+}
+/**
+ * betty comentarys
+ */
+listint_t *reversed_list(listint_t **head)
+{
+	listint_t *new_head, *tmp;
+
+	if((*head)->next == NULL)
+		return (*head);
+
+	new_head = reversed_list(&((*head)->next));
+	tmp = new_head;
+
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+	
+	tmp->next = *head;
+	(*head)->next = NULL;
+
+	return (new_head);
+}
+/**
+ * betty comentarys
+ */
+size_t len_list (listint_t **head)
+{
+	listint_t *tmp = *head;
+	size_t len = 0;
+
+	while(tmp != NULL)
+	{
+		tmp = tmp->next;
+		len++;
+	}
+	return (len);
+}
