80df461e43f21ff5e487975765517e4a2cf486ac
MiguelBarreraDev <barreram987@gmail.com>
2022-01-03 17:10:50 -0500

    Write a function in C that checks if a singly linked list is a palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -6,10 +6,10 @@ int is_palindrome(listint_t **head)
 {
 	listint_t *first, *end;
 	int band = 0; 
-	size_t len = 0, count = 0;
+	size_t len = 0, count = 0, save = 0;
 	
 	if (!head)
-		band = 1;
+		band = 0;
 	else if (*head == NULL)
 		band = 1;
 	else
@@ -17,15 +17,18 @@ int is_palindrome(listint_t **head)
 		first = *head;
 		end = reversed_list(head);
 		len = len_list(head);
-
-		while(first != NULL && end != NULL)
+		save = len;
+		len = (len % 2 == 0) ? len / 2 :  (len - 1) / 2;
+		
+		while(len)
 		{
 			if (first->n == end->n)
 				count++;
 			first = first->next;
 			end = end->next;
+			len--;
 		}
-		band = (count == len) ? 1 : 0;
+		band = (count == save) ? 1 : 0;
 	}
 	return (band); 
 }
@@ -38,18 +41,14 @@ listint_t *reversed_list(listint_t **head)
 	
 	if (!head || *head == NULL)
 		return (NULL);
-
 	if((*head)->next == NULL)
-	return (*head);
-
+		return (*head);
 	new_head = reversed_list(&((*head)->next));
 	tmp = new_head;
-
 	while (tmp->next != NULL)
-	tmp = tmp->next;	
+		tmp = tmp->next;	
 	tmp->next = *head;
 	(*head)->next = NULL;
-
 	return (new_head);
 }
 /**
@@ -62,7 +61,6 @@ size_t len_list (listint_t **head)
 	
 	if (!head || *head == NULL)
 		return (0);
-
 	while(tmp != NULL)
 	{
 		tmp = tmp->next;
