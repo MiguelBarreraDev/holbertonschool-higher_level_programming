1d8de770cbb2f2af76ddd92b89b03c000d22af05
MiguelBarreraDev <barreram987@gmail.com>
2022-01-03 18:45:25 -0500

    Write a function in C that checks if a singly linked list is a palindrome

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,8 +1,5 @@
 #ifndef LISTS_H
 #define LISTS_H
-
-#include <stdlib.h>
-
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -22,8 +19,4 @@ listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
-
-size_t len_list(listint_t **);
-listint_t *reversed_list(listint_t **);
-
 #endif
