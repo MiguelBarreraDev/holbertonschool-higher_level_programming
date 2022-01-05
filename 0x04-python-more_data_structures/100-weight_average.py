fe667fb4812928fc2a8dd4d9e5cd9d78c6a1d39c
MiguelBarreraDev <barreram987@gmail.com>
2022-01-05 14:15:44 -0500

    Write a function that returns the weighted average of all integers tuple (<score>, <weight>)

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if my_list == []:
+        return 0
     numerador = sum(list(map(lambda tpl: tpl[0] * tpl[1], my_list)))
     denominador = sum(list(map(lambda tpl: tpl[1], my_list)))
     average = numerador / denominador
