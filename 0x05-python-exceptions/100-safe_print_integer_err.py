00a05c4c7c65e176445d22b23930053712b699e0
MiguelBarreraDev <barreram987@gmail.com>
2022-01-17 17:16:53 -0500

    Write a function that prints an integer

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,7 +4,7 @@ def safe_print_integer_err(value):
     import sys
     try:
         print("{:d}".format(value))
+        return True
     except Exception as msg:
         print("Exception: {}".format(msg), file=sys.stderr)
         return False
-    return True
