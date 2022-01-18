e59602adefc06bde8cc61e6d2d5c892e25327f81
MiguelBarreraDev <barreram987@gmail.com>
2022-01-18 17:31:46 -0500

    Add my_print method

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,1 +1,38 @@
 #!/usr/bin/python3
+"""class - square"""
+
+
+class Square:
+    """
+    Define:
+        Property:
+            size
+        Methods:
+            area
+            my_print
+    """
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size=0):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for e in range(self.__size):
+                print('#' * self.__size)
