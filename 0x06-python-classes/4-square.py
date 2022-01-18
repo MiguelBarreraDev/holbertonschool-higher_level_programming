603ac116f4868a5686e6b15650aa50f285bcf678
MiguelBarreraDev <barreram987@gmail.com>
2022-01-18 17:29:30 -0500

    Add decoration(getters and setters)

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""class - square"""
+
+
+class Square:
+    """
+            Class that define
+    Properties:
+        size
+    Methods:
+        area
+    """
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size=0):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+    def area(self):
+        return self.__size ** 2
