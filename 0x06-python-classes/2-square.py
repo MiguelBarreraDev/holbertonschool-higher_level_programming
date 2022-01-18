3e648580f512deb47ea786c1000e41da4cb93e6f
MiguelBarreraDev <barreram987@gmail.com>
2022-01-18 17:23:19 -0500

    Validate property size of the class

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""class - square"""
+
+
+class Square:
+    """Class that validate the property size"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
