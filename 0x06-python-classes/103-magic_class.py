38a499e43f77be166307a3446e601625154dbaec
MiguelBarreraDev <barreram987@gmail.com>
2022-01-18 20:31:36 -0500

    ByteCode of the class - finally

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -5,17 +5,17 @@ import math
 
 class MagicClass:
     """Defines MagicClass"""
-    def __init__(self, raidus):
+    def __init__(self, raidus=0):
         """ Constructor """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
-    def area(self, radius):
+    def area(self, radius=0):
         """Get area"""
         return (self.__radius ** 2) * math.pi
 
-    def circunference(self, radius):
+    def circunference(self, radius=0):
         """Get circunference"""
         return 2 * math.pi * self.__radius
