8a4cb8479c20ba341a115a4761e5299399235646
MiguelBarreraDev <barreram987@gmail.com>
2022-01-18 20:34:22 -0500

    ByteCode of the class - finally

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -11,7 +11,8 @@ class MagicClass:
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
-        self.__radius = radius
+        else:
+            self.__radius = radius
 
     def area(self, radius):
         """Get area"""
