1f08c16ac8f32c3f06331690e67d3924a4c68f32
MiguelBarreraDev <barreram987@gmail.com>
2022-01-25 01:47:23 -0500

    Change exception name in bigger_or_equal method

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -61,9 +61,9 @@ class Rectangle(Dimension):
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         if not isinstance(rect_1, Rectangle):
-            raise Exception('rect_1 must be an instance of Rectangle')
+            raise TypeError('rect_1 must be an instance of Rectangle')
         if not isinstance(rect_2, Rectangle):
-            raise Exception('rect_2 must be an instance of Rectangle')
+            raise TypeError('rect_2 must be an instance of Rectangle')
         if rect_1.area() == rect_2.area():
             return rect_1
         return rect_1 if rect_1.area() > rect_2.area() else rect_2
