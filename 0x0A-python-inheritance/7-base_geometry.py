4e60c9f27b03e0ee04c87343ad10ca7e09830884
MiguelBarreraDev <barreram987@gmail.com>
2022-01-31 18:56:40 -0500

    Remove typing

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -12,7 +12,7 @@ class BaseGeometry:
     def area(self):
         raise Exception("area() is not implemented")
 
-    def integer_validator(self, name: str, value):
+    def integer_validator(self, name, value):
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
