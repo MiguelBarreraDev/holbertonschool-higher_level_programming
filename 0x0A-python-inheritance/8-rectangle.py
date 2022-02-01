91dba3e89a570e3672ec89ba303c90200b6065b5
MiguelBarreraDev <barreram987@gmail.com>
2022-01-31 19:07:43 -0500

    Remove typing for parameters function

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -9,10 +9,7 @@
 class BaseGeometry:
     """ Definition class
     """
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name: str, value):
+    def integer_validator(self, name, value):
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
