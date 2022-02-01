d29d1ba93cb4570dbc1a67dcf763a14d77baf239
MiguelBarreraDev <barreram987@gmail.com>
2022-01-31 21:31:57 -0500

    Write a function that adds a new attribute to an object if it’s possible:

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Use module for get attributes and methods of an object
 
-    Class:
+    function:
         lookup
 """
-def lookup(obj):
-    """ function definition that return a list object
-    """
-    return dir(obj)
+def add_attribute(obj, name, value):
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(obj, name, value)
