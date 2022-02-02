527ad9b3b9b741393b88ff59e9c87d129e6c4f05
MiguelBarreraDev <barreram987@gmail.com>
2022-02-01 21:01:03 -0500

    Log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -2,20 +2,12 @@
 """ Use the module for reads stdin line by line and computers metrics
 """
 
+
 import sys
+
 
 lim = 0
 arr = []
-try:
-    for line in sys.stdin:
-        arr.append(list(filter(lambda e: e != "-", line.split(" "))))
-        arr[lim] = arr[lim][-2:]
-        lim += 1
-        if lim == 10:
-            break
-except KeyboardInterrupt:
-    pass
-
 metrics = {
     "size": 0,
     "status": {
@@ -28,14 +20,30 @@ metrics = {
         "500": 0
     }
 }
+try:
+    """Get Data(status_code, size)"""
+    for line in sys.stdin:
+        arr.append(line.split())
+        arr[lim] = arr[lim][-2:]
+        lim += 1
+        if lim == 10:
+            break
 
-for line in arr:
-    if line[0] in metrics["status"].keys():
-        metrics["status"][line[0]] += 1
+    """Count status_code"""
+    for line in arr:
+        if line[0] in metrics["status"].keys():
+            metrics["status"][line[0]] += 1
 
-    metrics["size"] += int(line[1].replace("\n", ""))
+        metrics["size"] += int(line[1].replace("\n", ""))
 
-print("File size {:d}".format(metrics["size"]))
-for status in metrics["status"].keys():
-    if metrics["status"][status] != 0:
-        print("{}: {:d}".format(status, metrics["status"][status]), flush=True)
+    """Output format"""
+    print("File size {:d}".format(metrics["size"]))
+    for status in metrics["status"].keys():
+        if metrics["status"][status] != 0:
+            print("{}: {:d}".format(status, metrics["status"][status]))
+except KeyboardInterrupt:
+    print("File size {:d}".format(metrics["size"]))
+    for status in metrics["status"].keys():
+        if metrics["status"][status] != 0:
+            print("{}: {:d}".format(status, metrics["status"][status]))
+    raise
