15387788031f3fcc62402883b47f5058080c3ccf
MiguelBarreraDev <barreram987@gmail.com>
2022-02-07 13:48:24 -0500

    Add restriction in to_json_string method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -42,6 +42,8 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """ Parse to list object in json """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
