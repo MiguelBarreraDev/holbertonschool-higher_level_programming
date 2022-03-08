bc8a0416587cd3885bff0ed6314a190b4832f964
MiguelBarreraDev <barreram987@gmail.com>
2022-03-08 14:49:18 -0500

    Query for computes a average of all records

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- Script containing queries to interact with the database
+-- Query: Computes the score average of all records in the table second_table
+SELECT AVG(score) AS average FROM second_table;
