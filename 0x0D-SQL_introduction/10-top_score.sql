a9f04814fe711b867584fa1e82548b9a474b60e8
MiguelBarreraDev <barreram987@gmail.com>
2022-03-08 14:31:41 -0500

    Update(10-top_score.sql): Query command in uppercase

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -4,6 +4,6 @@
  * Display both the score and the name (int this order)
  * Records should be ordered bt score (top first)
  */
-SELECT score, name from second_table
+SELECT score, name FROM second_table
 ORDER BY score DESC;
 
