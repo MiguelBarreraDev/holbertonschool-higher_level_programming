320916c2acbc2180e6469e6c829ae696ecf61892
MiguelBarreraDev <barreram987@gmail.com>
2022-03-08 14:12:49 -0500

    Update(8-count_89.sql): Query in uppercase

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,4 +1,4 @@
 -- Script containing queries to interact with the database
 -- Query: Displays the number of records with id = 89
-SELECT COUNT(id) from first_table
+SELECT COUNT(id) FROM first_table
 WHERE id = 89;
