9f952e405204e8e4c6f6c63cb28decbf3a777bda
MiguelBarreraDev <barreram987@gmail.com>
2022-03-08 14:30:55 -0500

    Query for lists, display order specifie and order by score

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,9 @@
+-- Script containing queries to interact with the database
+/** 
+ * Lists all records of the table second_table
+ * Display both the score and the name (int this order)
+ * Records should be ordered bt score (top first)
+ */
+SELECT score, name from second_table
+ORDER BY score DESC;
+
