3c08f8392fc51650b7295924acc09045f4a6e601
MiguelBarreraDev <barreram987@gmail.com>
2022-03-08 15:12:31 -0500

    Update(15-groups.sql): Sorted by the number of records

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,5 @@
 -- Script containing queries to interact with the database
 -- Query: Lists the number of records with the same score
 SELECT score, COUNT(score) AS number FROM second_table
-GROUP BY score;
+GROUP BY score
+ORDER BY number DESC;
