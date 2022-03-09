bbc48df0003088a4609642d3b81fe935dc13eb81
MiguelBarreraDev <barreram987@gmail.com>
2022-03-09 12:17:53 -0500

    Update(15-comedy_only.sql) Remove identation begin

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -7,8 +7,8 @@
  */
 SELECT s.title FROM tv_shows s
 JOIN tv_show_genres sg
-	ON s.id = sg.show_id
+ON s.id = sg.show_id
 JOIN tv_genres g
-	ON g.id = sg.genre_id
+ON g.id = sg.genre_id
 WHERE g.name = 'Comedy'
 ORDER BY s.title 
