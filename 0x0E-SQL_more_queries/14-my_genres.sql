36e4055b430d2f9581103e316879f18e1f6d9b5a
MiguelBarreraDev <barreram987@gmail.com>
2022-03-09 12:23:07 -0500

    Update(14-my_genres.sql): Reove multline Comments

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,10 +1,5 @@
 -- Commans to managment a database
 -- Query: Uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
-/**
- * The tv_shows table contains only one record where title = Dexter
- * Each record should display tv_genres.name
- * Sorted in ascending order by the genre name
- */
 SELECT tv_genres.name FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
