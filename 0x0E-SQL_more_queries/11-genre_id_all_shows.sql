a5c9845c29aa050f4fbbb8f09c0751c2c1d3d176
MiguelBarreraDev <barreram987@gmail.com>
2022-03-09 12:32:05 -0500

    Remove multilines comments

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,10 +1,5 @@
 -- Commans to managment a database
 -- Lists all shows contained in the database hbtn_0d_tvshows
-/**
- * Each record should display tv_shows.title - tv_show_genres.genre_id
- * Sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
- * If a show doesn't have a genre, display NULL
- */
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
