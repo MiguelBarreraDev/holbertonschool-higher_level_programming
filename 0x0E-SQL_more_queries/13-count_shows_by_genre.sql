9fae468cc9a46f4dd5ddefe41d29d57fe5f47510
MiguelBarreraDev <barreram987@gmail.com>
2022-03-09 12:23:45 -0500

    Remove multiline comments

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,13 +1,6 @@
 -- Commans to managment a database
 -- Query: Lists all genres from hbtn_0d_tsvhows and displays the number od shows
 -- linked to each
-/**
- * Each record should display <TV Show genre> - <Number of shows linked to this genre>
- * First column must be called genre
- * Second column must be called number_of_shows
- * Don't display a genre that doesn't have any show linked
- * Sorted in descending order by the number of shows linked
- */
 SELECT 
 	tv_genres.name AS genre, 
 	COUNT(tv_show_genres.genre_id) AS number_of_shows
