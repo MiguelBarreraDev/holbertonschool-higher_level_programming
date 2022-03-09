322c839ba2dd6abed052195f3671eb0cb724b68f
MiguelBarreraDev <barreram987@gmail.com>
2022-03-09 12:29:30 -0500

    Add 'ORDER BY' sentence

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,8 +1,8 @@
 -- Commans to managment a database
 -- Query: Uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
-SELECT tv_genres.name FROM tv_genres
+SELECT g.name FROM tv_genres g
 INNER JOIN tv_show_genres
-ON tv_genres.id = tv_show_genres.genre_id
+ON g.id = tv_show_genres.genre_id
 INNER JOIN tv_shows
 ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_shows.title = 'Dexter'
