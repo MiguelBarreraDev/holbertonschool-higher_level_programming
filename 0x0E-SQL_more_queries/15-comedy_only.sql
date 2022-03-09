ba72363dc4d919a53daeac3d013d3f905a1c6e1e
MiguelBarreraDev <barreram987@gmail.com>
2022-03-09 12:22:01 -0500

    Update(15-comedy_only.sql): Comments

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,10 +1,5 @@
 -- Commands to managment a database
-/*
- * Query: Lists all Comedy shows in the database hbtn_0d_tvshows
- * The tv_genres table contains only one record where name = Comedy
- * Each record should display tv_shows.title
- * Sorted in ascending order by the show title
- */
+-- Query: Lists all Comedy shows in the database hbtn_0d_tvshows
 SELECT s.title FROM tv_shows s
 JOIN tv_show_genres sg
 ON s.id = sg.show_id
