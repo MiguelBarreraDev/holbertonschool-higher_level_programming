f98b6e54cbbf9de710b11293dc498dca764b208a
MiguelBarreraDev <barreram987@gmail.com>
2022-03-09 21:04:23 -0500

    Query for lists all shows without the genre Comedy in the database

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,1 +1,14 @@
--- Commans to managment a database
+-- Commands to managment a database
+-- Query: Use the hbtn_0d_tvshows database to list all genres
+-- not linked to the show Dexter
+
+SELECT s.title FROM tv_shows s
+WHERE s.title NOT IN (
+	SELECT s.title FROM tv_shows s
+	LEFT JOIN tv_show_genres sg
+	ON s.id = sg.show_id
+	LEFT JOIN tv_genres g
+	ON g.id = sg.genre_id
+	WHERE g.name = 'Comedy'
+)
+ORDER BY s.title;
