0f09b78c130800d1218038235754e90eef02b780
MiguelBarreraDev <barreram987@gmail.com>
2022-03-09 21:29:15 -0500

    Query for ists all genres in the database by their rating

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,1 +1,11 @@
 -- Commans to managment a database
+-- Query: Lists all genres in the database hbtn_0d_tvshows_rate by their rating
+SELECT g.name, SUM(sr.rate) AS rating FROM tv_genres g
+JOIN tv_show_genres sg
+ON g.id = sg.genre_id
+JOIN tv_shows s
+ON s.id = sg.show_id
+JOIN tv_show_ratings sr
+ON sr.show_id = s.id
+GROUP BY g.name
+ORDER BY rating DESC
