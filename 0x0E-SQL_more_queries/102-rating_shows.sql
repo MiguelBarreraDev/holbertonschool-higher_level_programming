25ef5082b23f222bc021f15bd438fd79012bdf3d
MiguelBarreraDev <barreram987@gmail.com>
2022-03-09 21:15:30 -0500

    Query for ists all shows from hbtn_0d_tvshows_rate by their rating

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,1 +1,7 @@
--- Commans to managment a database
+-- Commands to managment a database
+-- Query: Lists all shows from hbtn_0d_tvshows_rate by their raiting
+SELECT s.title, SUM(sr.rate) AS sum FROM tv_shows s
+JOIN tv_show_ratings sr
+ON s.id = sr.show_id
+GROUP BY s.title
+ORDER BY sum DESC;
