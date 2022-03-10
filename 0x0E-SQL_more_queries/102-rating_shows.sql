a972e6e24cb8a0c9f93f26cbc975623c133cd914
MiguelBarreraDev <barreram987@gmail.com>
2022-03-09 21:18:29 -0500

    Add: Alias for column raiting

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- Commands to managment a database
 -- Query: Lists all shows from hbtn_0d_tvshows_rate by their raiting
-SELECT s.title, SUM(sr.rate) AS raiting FROM tv_shows s
+SELECT s.title, SUM(sr.rate) AS rating FROM tv_shows s
 JOIN tv_show_ratings sr
 ON s.id = sr.show_id
 GROUP BY s.title
