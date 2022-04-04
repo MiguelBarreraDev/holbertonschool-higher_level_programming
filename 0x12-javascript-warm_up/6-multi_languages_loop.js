20d683d850cc87398bd354f82b537704fa1b2783
MiguelBarreraDev <barreram987@gmail.com>
2022-04-04 14:39:25 -0500

    Remove semicolon

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const content = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const content = ['C is fun', 'Python is cool', 'JavaScript is amazing']
 
 for (let i = 0; content[i]; i++) {
-  console.log(content[i]);
+  console.log(content[i])
 }
