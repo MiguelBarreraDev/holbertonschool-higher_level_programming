e9fed0057573a82067ec0870846403abd9af4fcf
MiguelBarreraDev <barreram987@gmail.com>
2022-04-04 14:51:23 -0500

    Use forEach method

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
 
 const content = ['C is fun', 'Python is cool', 'JavaScript is amazing']
-
-for (const i = 0; content[i]; i++) {
-  console.log(content[i])
-}
+content.forEach((text) => console.log(text));
