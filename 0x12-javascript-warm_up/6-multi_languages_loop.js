48f8cf09761daad45d65321f57fd5722af6ebd19
MiguelBarreraDev <barreram987@gmail.com>
2022-04-04 14:29:42 -0500

    Use iterator in for

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
+
 const content = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (const itr in content) {
-  console.log(content[itr]);
+
+for (let i = 0; content[i]; i++) {
+  console.log(content[i]);
 }
