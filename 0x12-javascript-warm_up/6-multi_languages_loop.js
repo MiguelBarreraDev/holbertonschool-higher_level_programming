898876f0f0f1c74894dbd967cae66d736fe4e80a
MiguelBarreraDev <barreram987@gmail.com>
2022-04-04 17:56:02 -0500

    prints 3 lines

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-
 const content = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-content.forEach((text) => console.log(text));
+for (let i = 0; i < content.length; i++) {
+  console.log(content[i]);
+}
