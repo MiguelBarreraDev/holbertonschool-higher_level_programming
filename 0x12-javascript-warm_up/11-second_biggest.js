4c30d9bc567990bb48afd46b3ed651492a8a8036
MiguelBarreraDev <barreram987@gmail.com>
2022-04-04 15:22:21 -0500

    use sort method

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
-const listOfNumbers = process.argv.splice(2).map((e) => parseInt(e)).sort();
+const listOfNumbers = process.argv.splice(2).map(
+  (e) => parseInt(e)
+).sort((a, b) => a > b);
 if (listOfNumbers.length <= 1) console.log(0);
 else console.log(listOfNumbers[listOfNumbers.length - 2]);
