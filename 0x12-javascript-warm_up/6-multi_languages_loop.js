672689ba86b358c890c69ed812a99090f171b999
MiguelBarreraDev <barreram987@gmail.com>
2022-04-04 14:52:14 -0500

    Add semicolon

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
-const content = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+const content = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 content.forEach((text) => console.log(text));
