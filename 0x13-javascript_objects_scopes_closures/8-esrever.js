e77bef9e5f2a8371ed9d852d03dec7ac80986e8f
MiguelBarreraDev <barreram987@gmail.com>
2022-04-06 02:41:31 -0500

    Use arrow function in oneline

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,6 +1,2 @@
 #!/usr/bin/node
-exports.esrever = function (list) {
-  const invertedList = [];
-  for (const e of list) invertedList.unshift(e);
-  return invertedList;
-};
+exports.esrever = (list) => [...list].map(() => list.pop());
