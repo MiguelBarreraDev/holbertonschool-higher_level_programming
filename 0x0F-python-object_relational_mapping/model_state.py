d3573b48912f3bdb8e1a90e1a2a4658f7349a289
MiguelBarreraDev <barreram987@gmail.com>
2022-04-09 15:23:50 -0500

    Add: Docstring for the class and moduel

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
+"""
+Module that defines the model for a table
+
+Classes
+-------
+    State
+"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-"""
-Module description
-"""
 
 Base = declarative_base()
 
 
 class State(Base):
     """
-    Class definition
+    State table model in the database.
+    Allows the creation and insertion of records
     """
     __tablename__ = 'states'
-    id = Column(
-        Integer, nullable=False, autoincrement='ignore_fk', primary_key=True
-    )
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
