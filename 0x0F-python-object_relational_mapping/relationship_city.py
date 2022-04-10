86dcb2744c6e14bb50e87775beb1754105891671
MiguelBarreraDev <barreram987@gmail.com>
2022-04-10 17:25:05 -0500

    Add: models of the tables

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""
+Module that defines the model for a table
+
+Classes
+-------
+    City
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.sql.schema import ForeignKey
+from relationship_state import Base
+
+
+class City(Base):
+    """
+    City table model in the database.
+    Allows the creation and insertion of records
+    """
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"))
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""
+Module that defines the model for a table
+
+Classes
+-------
+    State
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    State table model in the database.
+    Allows the creation and insertion of records
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    cities = relationship('City')
+
+    def __init__(self, name):
+        self.name = name
