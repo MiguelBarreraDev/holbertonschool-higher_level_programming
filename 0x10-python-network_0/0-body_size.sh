68d501ff422e370956e930f7d36a13ae14dca6b1
MiguelBarreraDev <barreram987@gmail.com>
2022-04-21 14:53:40 -0500

    Add shebang

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 # Write a Bash script that takes in a URL, sends a request to that URL,
 # and displays the size of the body of the response
 curl -I -s $1 | grep "Content-Length" | cut -d " " -f 2
