96aad35c532eda08f2e0d8b6418d9e6d7eef7359
MiguelBarreraDev <barreram987@gmail.com>
2022-04-21 17:10:12 -0500

    Add file

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Write a Bash script that sends a JSON POST request to a URL
-curl -s -X POST -H "Content-Type: application/json" -F 'data=$2' $1
+curl -s -X POST -H "Content-Type: application/json" -d @$2 $1
