891d860bdde6fc4db33c9c33206a9957e3b4c427
MiguelBarreraDev <barreram987@gmail.com>
2022-04-22 13:28:04 -0500

    Your code should not be executed when imported

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,16 +3,17 @@
 Sends a POST request to the passed URL with the email as a parameter,
 and displays the body of the response (decoded in utf-8)
 """
-import urllib.request
-import urllib.parse
-from sys import argv
+if __name__ == "__main__":
+    import urllib.request
+    import urllib.parse
+    from sys import argv
 
-[url, email] = argv[1:]
-params = {"email": email}
-data = urllib.parse.urlencode(params)
-data = data.encode("ascii")  # Data should be bytes
-req = urllib.request.Request(url, data)
+    [url, email] = argv[1:]
+    params = {"email": email}
+    data = urllib.parse.urlencode(params)
+    data = data.encode("ascii")  # Data should be bytes
+    req = urllib.request.Request(url, data)
 
-# With data argument the request will use POST method
-with urllib.request.urlopen(req) as res:
-    print(res.read().decode("utf-8"))
+    # With data argument the request will use POST method
+    with urllib.request.urlopen(req) as res:
+        print(res.read().decode("utf-8"))
