786d76ea432d9fd519365f7304294260021f61a7
MiguelBarreraDev <barreram987@gmail.com>
2022-04-22 13:54:30 -0500

    Write a Python script that fetches https://intranet.hbtn.io/status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+Write a Python script that fetches https://intranet.hbtn.io/status
+"""
+
+if __name__ == "__main__":
+    import requests
+    url = "https://intranet.hbtn.io/status"
+    res = requests.get(url)
+    print("Body Response")
+    print("\t- type:", type(res.text))
+    print("\t- content:", res.text)
