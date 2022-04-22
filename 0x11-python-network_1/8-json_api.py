a7255bfe43ba5fae2085b4b24f553d385d135ab9
MiguelBarreraDev <barreram987@gmail.com>
2022-04-22 15:00:33 -0500

    sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""
+Sends a POST request to http://0.0.0.0:5000/search_user with the
+letter as a parameter
+"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    url = "http://0.0.0.0:5000/search_user"
+    data = {
+        "q": "" if len(argv) < 2 else argv[1]
+    }
+
+    res = requests.post(url, data=data)
+    try:
+        res = res.json()
+        if res == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(res.get("id"), res.get("name")))
+    except Exception:
+        print("Not a valid JSON")
