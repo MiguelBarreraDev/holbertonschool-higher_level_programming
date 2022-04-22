f97e1bce5e6a823c15d39920d999efce139f3b4d
MiguelBarreraDev <barreram987@gmail.com>
2022-04-22 16:02:20 -0500

    uses the GitHub API to display your id

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+Uses the GitHub API to display your id
+"""
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    url_api = "https://api.github.com/user"
+
+    res = requests.get(url_api, auth=(argv[1], argv[2]))
+    res = res.json()
+    print(res.get("id"))
