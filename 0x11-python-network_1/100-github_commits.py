a4b624c7055975cac2764c362a193a19615da57e
MiguelBarreraDev <barreram987@gmail.com>
2022-04-22 16:57:05 -0500

    Please list 10 commits (from the most recent to oldest) of the repository “rails” by the user “rails”

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""
+The Holberton School staff evaluates candidates applying for a
+back-end position with multiple technical challenges, like this one:
+"""
+
+
+def api_github(path="", params=""):
+    """ Request to api of github """
+    import requests
+    api_github = "https://api.github.com"
+    url = api_github + "/" + path + params
+    res = requests.get(url)
+    return res
+
+
+if __name__ == "__main__":
+    from sys import argv
+
+    [repo, owner] = argv[1:]
+    
+    commits = api_github("repos/{}/{}/commits?per_page=10".format(owner, repo))
+    commits = commits.json()
+    for commit in commits:
+        print("{}: {}".format(
+            commit.get("sha"),
+            commit.get("commit").get("author").get("name")
+        ))
