cbd675a79d1c342c427c9eaf64fe4c4be1306c8d
MiguelBarreraDev <barreram987@gmail.com>
2022-05-17 16:20:51 -0500

    Refactor code

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,6 +5,8 @@ const [url, filePath] = process.argv.slice(2);
 
 axios.get(url)
   .then(({ data }) => {
-    fs.writeFile(filePath, data, 'utf-8', err => console.log(err));
+    fs.writeFile(filePath, data, 'utf-8', err => {
+      if (err) console.log(err.message);
+    })
   })
-  .catch(err => console.log(err));
+  .catch(err => console.log(err.message));
