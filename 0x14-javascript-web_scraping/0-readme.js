d66a6eacace56f5c10578b9f9e5d637f21ae28e6
MiguelBarreraDev <barreram987@gmail.com>
2022-05-17 14:42:19 -0500

    Update

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,6 +2,7 @@
 const fs = require('fs');
 const fileName = process.argv[2];
 
-fs.promises.readFile(fileName, 'utf8')
-  .then(res => console.log(res))
-  .catch(err => console.log(err));
+fs.readFile(fileName, 'utf8', (err, res) => {
+  if (err) console.log(err);
+  else console.log(res);
+});
