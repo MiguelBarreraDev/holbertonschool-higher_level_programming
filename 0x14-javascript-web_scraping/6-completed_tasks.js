3fb637158c94d719b614b64485c690523a51d022
MiguelBarreraDev <barreram987@gmail.com>
2022-05-17 17:17:34 -0500

    Add: script that computes the number of tasks completed by user id

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const axios = require('axios');
+const url = process.argv[2];
+
+axios.get(url)
+  .then(({ data }) => {
+    const taskForUser = {};
+    data.forEach(e => {
+      if (taskForUser[e.userId] === undefined) taskForUser[e.userId] = 0;
+      if (e.completed) taskForUser[e.userId]++;
+    });
+    console.log(taskForUser);
+  })
+  .catch(err => console.log(err.message));
