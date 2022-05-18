53f5c0db7a56948355489d67f1a9667683caf6a3
MiguelBarreraDev <barreram987@gmail.com>
2022-05-18 02:51:43 -0500

    Modify: Refactor code

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const axios = require('axios');
 const apiUrl = process.argv[2];
-const result = {};
+const [result, endResult] = [{}, {}];
 
 axios.get(apiUrl)
   .then(({ data }) => {
@@ -9,6 +9,9 @@ axios.get(apiUrl)
       if (result[task.userId] === undefined) result[task.userId] = 0;
       if (task.completed) result[task.userId]++;
     });
-    console.log(result);
+    Object.keys(result).forEach(key => {
+      if (result[key]) endResult[key] = result[key];
+    });
+    console.log(endResult);
   })
   .catch(({ message }) => console.log(message));
